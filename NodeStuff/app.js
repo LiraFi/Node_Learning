52855d44239e34aae3bd0fc591224ef06b4c725a
LiraFi <iryna.lakiza@gmail.com>
2023-01-30 12:11:09 +0200

    modified reading added

--- NodeStuff/app.js
+++ NodeStuff/app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
-fs.readFile('./text.txt', (error, data) => {
-  console.log(data.toString());
+fs.readFile('./text.txt', 'utf8', (error, data) => {
+  console.log(data);
 });
+
+console.log('Just text!');

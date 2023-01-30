19b935549d73714616b7f30a64d54c0667605356
LiraFi <iryna.lakiza@gmail.com>
2023-01-30 12:45:03 +0200

    added if .existsSync added

--- NodeStuff/app.js
+++ NodeStuff/app.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 fs.readFile('./text.txt', 'utf8', (error, data) => {
-  fs.writeFile('./files/text2.txt', `${data} New text added!`, (error) => {
-    error ? console.log(error) : null;
+  fs.mkdir('./files', () => {
+
+    fs.writeFile('./files/text2.txt', `${data} New text added!`, (error) => {
+      error ? console.log(error) : null;
+    });
   });
 });
+
+setTimeout(() => {
+  if (fs.existsSync('./files/text2.txt')) {
+    fs.unlink('./files/text2.txt', () => { });
+  }
+}, 4000);
+
+setTimeout(() => {
+  if (fs.existsSync('./files')) {
+    fs.rmdir('./files', () => { });
+  }
+}, 6000);
 

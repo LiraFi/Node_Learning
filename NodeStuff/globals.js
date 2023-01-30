b961340a7f6ea70fd8c64f9494103b999aa032b8
LiraFi <iryna.lakiza@gmail.com>
2023-01-30 11:41:22 +0200

    trying out different

---
+++ NodeStuff/globals.js
@@ -0,0 +1,12 @@
+//console.log(globals);
+
+//setTimeout(() => {
+//console.log("Hi!");
+//}, 2000);
+
+//console.log(__dirname);
+
+//console.log(__filename);
+
+//console.log(process.argv);
+console.log(`Hi, ${process.argv[2]}`); //.env; .argv
---
+++ NodeStuff/test.js
@@ -0,0 +1,6 @@
+const userName = "Eva";
+
+const sayHi = (userName) => `Hello, my name is ${userName}`;
+
+console.log(sayHi(userName));
+
